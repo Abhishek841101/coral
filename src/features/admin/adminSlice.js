ef772e80af011b90e5b2e6d6112e4c8bd085e6e1
Abhishek Kumar <abhishek841101@gmail.com>
2026-09-01 12:58:53 +0530

    change api

--- src/features/admin/adminSlice.js
+++ src/features/admin/adminSlice.js
@@ -3,7 +3,7 @@ import {
   createSlice,
 } from "@reduxjs/toolkit";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 /* =====================================================
    HELPER
--- src/features/bookings/bookingSlice.js
+++ src/features/bookings/bookingSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 /* =====================================================
    CREATE BOOKING
